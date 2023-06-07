65f682682fe5731ac1ece896d5f77d35f4298f10
GoshaSilaev <90410842+GoshaSilaev@users.noreply.github.com>
2023-06-07 18:46:33 +0300

    1

---
+++ src/view/point-list-empty-view.js
@@ -0,0 +1,23 @@
+import AbstractView from '../framework/view/abstract-view.js';
+import { FilterType } from '../const.js';
+
+const messages = {
+  [FilterType.EVERYTHING]: 'Click New Event to create your first point',
+  [FilterType.FUTURE]: 'There are no future events now',
+};
+
+const createEmptyListMessageTemplate = (filterType) =>
+  `<p class="trip-events__msg">${messages[filterType]}</p>`;
+
+export default class EmptyPointListView extends AbstractView {
+  #filterType = null;
+
+  constructor (filterType) {
+    super();
+    this.#filterType = filterType;
+  }
+
+  get template() {
+    return createEmptyListMessageTemplate(this.#filterType);
+  }
+}
